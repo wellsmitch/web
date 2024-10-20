f496e65263dc00b52d4d551a5ed3764d154e4336
wellsmitch <42932079+wellsmitch@users.noreply.github.com>
2024-10-20 22:58:41 +0800

    Update love.js

--- js/love.js
+++ js/love.js
@@ -168,8 +168,8 @@
 
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
-            ctx.font = "12px 微软雅黑,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
-            ctx.fillText("倩倩 点这里", 23, 16);
+            ctx.font = "16px 微软雅黑,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
+            ctx.fillText("宁宁宁 点这里", 23, 16);
             ctx.restore();
         },
         clear: function() {
